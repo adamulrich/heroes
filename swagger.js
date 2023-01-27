bac358a7c06dc55ffdbfb576caac1f97c56f8b00
adamulrich <adamulrich@hotmail.com>
2023-01-27 01:10:54 -0800

    wip

--- swagger.js
+++ swagger.js
@@ -59,6 +59,42 @@ const doc = {
       url: "https://static.wikia.nocookie.net/starwars/images/4/43/TheChild-Fathead.png/revision/latest?cb=20201031231040"
     }
   },
+  addHero: {
+    name: "Grogu",
+    powerstats: {
+      intelligence: 40,
+      strength: 30,
+      speed: 33,
+      durability: 25,
+      power: 50,
+      combat: 50
+    },
+    biography: {
+      "full-name": "Grogu",
+      "alter-egos": "No alter egos found.",
+      aliases: ["The Child", "Baby Yoda"],
+      "place-of-birth": "-",
+      "first-appearance": "The Mandalorian (2019)",
+      publisher: "George Lucas",
+      alignment: "good"
+    },
+    appearance: {
+      gender: "Male",
+      race: "Yoda's species",
+      height: ["1'1", "34 cm"],
+      weight: ["19 lb", "17 kg"],
+      "eye-color": "Brown",
+      "hair-color": "White"
+    },
+    work: { occupation: "-", "base": "-" },
+    connections: {
+      "group-affiliation": "",
+      relatives: ""
+    },
+    image: {
+      url: "https://static.wikia.nocookie.net/starwars/images/4/43/TheChild-Fathead.png/revision/latest?cb=20201031231040"
+    }
+  },
   heroNameAndId: {
     id: 0,
     name: "Grogu"
